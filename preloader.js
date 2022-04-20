04a503be92e1e2d352bb90013e7ff8597b45ae9e
AlanJunior <alanjr162006@gmail.com>
2022-04-20 13:01:25 +0630

    FinalResult

---
+++ preloader.js
@@ -0,0 +1,8 @@
+// Preloader
+
+let preloader = document.querySelector("#preloader");
+window.addEventListener("load", () => {
+    preloader.style.display="none";
+});
+
+// Preloader
---
+++ script.js
@@ -0,0 +1,12 @@
+
+// LinkPage
+
+let donateButton = document.querySelector("#donateButton");
+let donatePageDiv = document.querySelector(".donatePageDiv");
+
+donateButton.addEventListener("click", () => {
+    donatePageDiv.classList.remove("hide");
+});
+
+// LinkPage
+
